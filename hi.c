b157dd7b7bdef093bbb18bb59187f0bea2a0b89d
Black-Butterfly-Codes <blackbutterflycodes@gmail.com>
2026-06-11 12:46:37 +0530

    Update hi.c

---
+++ hi.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main ()
+{
+printf("> Welcome to my first GitHub repository! \nThis demo repository was created as part of my GitHub learning journey to explore GitHub features, practice Git commands, test commits and branches, and learn version control workflows. 
+");
+return 0 ;
+}
